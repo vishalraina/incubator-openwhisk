e3b18448ed85eeedb288f840c4ed8661cbb90d02
Vishal Raina <viraina@MacBook-Pro.local>
2017-10-19 22:17:33 -0700

    Ansible and java install bandaid to make Vangrant work for openwisk setup.

--- tools/ubuntu-setup/ansible.sh
+++ tools/ubuntu-setup/ansible.sh
@@ -7,6 +7,9 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y python-dev libffi-dev libssl-dev
 sudo pip install markupsafe
+#Ansiblae install fails due to crypo issue here is thread which helped to solve it https://github.com/ansible/ansible/issues/31741
+sudo pip install --upgrade setuptools
+sudo apt-get install libffi-dev libssl-dev
 sudo pip install ansible==2.3.0.0
 sudo pip install docker==2.2.1
 sudo pip install jinja2==2.9.6
